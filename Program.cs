ba23542ccdddb2a8661529b2f2474baf0983f8d6
Vladimir Bubenko <bawmc@mail.ru>
2023-03-09 22:44:09 +0700

    Done Print

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// написать программу, которая из имеющегося массива строк, формирует массив из строк
+// длина которых меньше либо равна 3 символа
+string [] array = new string [4] {"hello", "2", "world", ":)"};
+void CheckArray (string [] array)
+{
+    string [] array2 = new string [array.Length];
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+            {
+                array2[count] = array[i];
+                count++;
+            }
+    }
+}
+
+void PrintArray (string [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+    Console.WriteLine();
+}
+
+PrintArray(array);
